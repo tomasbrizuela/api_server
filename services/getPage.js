fb5424fd9d94d08d3d07558ede96384d7208add9
Tomi <tomasagustinbrizuela@gmial.com>
2025-02-01 11:39:21 -0300

    added log

--- services/getPage.js
+++ services/getPage.js
@@ -7,8 +7,7 @@ router.post('/', async (req, res) => {
         const response = await fetch("https://ipinfo.io/json", {});
         console.log(response)
         const json = await response.json();
-
-
+        console.log()
         return res.status(200).json({ "Message": json })
     } catch (error) {
         return res.status(400).json({ "Error": error })
